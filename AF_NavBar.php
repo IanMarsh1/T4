39c1715a9350fb6685ded3cb18fc0f42495de81a
AntL552 <Antonio.Lopez2@marist.edu>
2021-11-03 15:13:15 -0400

    oh hey look a nav bar
    you change the colors

---
+++ AF_NavBar.php
@@ -0,0 +1,53 @@
+<!-- 
+	THIS SHIT IS KINDA WONKY
+	SIZING IS NOT COMPATIBLE I THINK
+ -->
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<title> Header Bar </title>
+		<meta charset="utf-8">
+		
+		<style>
+			ul {
+			  float: left;
+			  list-style-type: none;
+			  margin: 0;
+			  padding: 0;
+			  width: 200px;
+			  background-color: #f1f1f1;
+				}
+
+			li a {
+			  float: left;
+			  display: block;
+			  color: #000;
+			  padding: 8px 4px;
+			  text-decoration: none;
+			}
+
+			li a:hover {
+			  background-color: #555;
+			  color: white;
+			}
+		
+		</style>
+		
+	</head>
+	
+	<body>
+	
+		<h2>Vertical Navigation Bar</h2>
+
+		<ul>
+		  <li><a href="#home">Home</a></li>
+		  <li><a href="#news">News</a></li>
+		  <li><a href="#contact">Contact</a></li>
+		  <li><a href="#about">About</a></li>
+		</ul>
+	
+	</body>
+	
+	
+</html>
