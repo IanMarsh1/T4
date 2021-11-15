52e11239864009a7705dddbdd70136638619465d
AntL552 <Antonio.Lopez2@marist.edu>
2021-11-15 18:02:24 -0500

    Added Active to Table
    So that my code for the suppliers table actually works

--- Team4_Tables.sql
+++ Team4_Tables.sql
@@ -76,6 +76,7 @@ create table if not exists T4_Suppliers (
 	supplierEmail varchar(30) not null default('N/A'),
 	supplierPhoneNumber varchar(30) default('N/A'),
 	address text default('N/A')
+	active enum('Y','N') default 'Y';
 );
 
 -- Using a real-world snowboard supplier as an example in the database.
