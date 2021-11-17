114d87840ac57e1c1b0b008efd3bfb4da5edc84d
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2021-11-17 14:29:20 -0500

    Update AF_ShowItemsTable.php

--- AF_ShowItemsTable.php
+++ AF_ShowItemsTable.php
@@ -185,7 +185,7 @@
             echo "<td> " . $row[3] ."</td>";
             echo "<td> " . $row[4] ."</td>";
             echo "<td> " . $row[8] ."</td>";
-            echo "<td> <a href = 'http://localhost/T4/deleteInTable.php?id=$row[0]&active=N'> Delete </a></td>";
+            echo "<td> <a href = 'http://localhost/T4/AF_ItemsDelete.php?id=$row[0]&active=N'> Delete </a></td>";
                 
             echo "</tr>"; 
         }
