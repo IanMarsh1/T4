e736ee356024da0c5ee4cd012870b38e50c5eb6a
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2021-11-20 15:44:34 -0500

    changed footer

--- AF_Footer.php
+++ AF_Footer.php
@@ -11,10 +11,10 @@
 //define("FILE_AUTHOR","Antonio Lopez, Luke Pecovic, and Ian Marsh");
 echo "<footer style='background-color:#036933;'>";  
 echo '<small style="color:#dfe7d3";> Need Help? Send us an Email! <a herf=<a href="mailto:Ian.Marsh1@marist.edu, Luke.Pecovic1@marist.edu, antonio.lopez2@marist.edu" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">ArcticFox Support Team</a>';
-echo '<br><a href="AF_DisclaimerPage.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Disclaimer Link</a>';
-echo '<br><a href="AF_Construction.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Privacy Statement Link</a>';
-echo '<br><a href="AF_Construction.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Rental/ Return Policy Link</a>';
-echo '<br><a href="AF_ChangeLog.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Change Log Link</a>';
+echo '<br><a href="AF_DisclaimerPage.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Disclaimer</a>';
+echo '<br><a href="AF_Construction.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Privacy Statement</a>';
+echo '<br><a href="AF_Construction.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Rental/ Return Policy</a>';
+echo '<br><a href="AF_ChangeLog.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Change Log</a>';
 echo "<small style='color:#dfe7d3';> Current Version 1.1.0 </small>";
 echo '<br><small style="color:#dfe7d3";>This page © by ArcticFox, 2021 </small>';
 echo "<br><small style='color:#dfe7d3';>Designed and created by ".FILE_AUTHOR.".</small>";
