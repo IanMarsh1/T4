c801b3a37fc171a939b8897a8777a9d7cd354383
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2021-12-01 13:57:39 -0500

    Fix nav

--- AF_NavBar.php
+++ AF_NavBar.php
@@ -45,3 +45,32 @@ if (isset($_SESSION['login_status'])) {
         echo '</table>';
     }
 }
+else {
+
+    echo '<table border = 1; 
+    style= "width:100%;
+    background-color: #036933;
+    color: black;
+    border-collapse: collapse;
+    border: 1px;
+    font-family: serif;
+    font-weight: normal;
+    margin-right: auto;
+    margin-left: auto;"
+    >';
+
+    echo    '<col style="width:25%">';
+    echo     '<col style="width:25%">';
+    echo    '<col style="width:25%">';
+    echo    '<col style="width:25%">';
+
+    //		<!-- Info on the website -->
+    echo        '<tr>';
+    echo        '<th style = "background-color:#036933";><a href="AF_ShowItemsTable.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Items</a></th>';
+    echo        '<th style = "background-color:#036933";><a href="AF_ShowSuppliersTable.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Suppliers</a></th>';
+    echo        '<th style = "background-color:#036933";><a href="AF_LoginPage.php" style = "color: #dfe7d3;background-color: transparent;text-decoration: none;font-size: 18px;">Employee Login</a></th>';
+    echo        '<th style = "background-color:#036933";><div class="topnav" text-align: center;><input type="text" placeholder="Search.."></div></th>';
+    echo    '</tr>';
+
+    echo '</table>';
+}
